7d246384b139175d00fca4fa992aee415aa64204
Tobi Odogwu (Tobi Banks) <63855026+tobibanks@users.noreply.github.com>
2023-10-27 17:06:35 +0100

    nawa o

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyB9mcUyk4Lezl8wUpevVSNMA8D6YuXveDA",
+        authDomain: "clips-431ca.firebaseapp.com",
+        projectId: "clips-431ca",
+        storageBucket: "clips-431ca.appspot.com",
+        messagingSenderId: "950699718320",
+        appId: "1:950699718320:web:2fe0b9bdef12fa2886ad75"
+      }
+};
