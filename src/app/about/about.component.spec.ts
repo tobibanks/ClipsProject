3aa209a072478856a222f09e807ca84a34edc567
tobibanks <tobiodogwu@gmail.com>
2024-01-02 00:02:21 +0100

    pushing again

--- src/app/about/about.component.spec.ts
+++ src/app/about/about.component.spec.ts
@@ -1,21 +1,27 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {  TestBed, ComponentFixture } from '@angular/core/testing';
 import { AboutComponent } from './about.component';
 
-describe('AboutComponent', () => {
-  let component: AboutComponent;
+
+describe('About Component', () => {
   let fixture: ComponentFixture<AboutComponent>;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [AboutComponent]
-    });
+  let component: AboutComponent;
+  
+  beforeEach( async ()=>{
+   await TestBed.configureTestingModule({
+      declarations: [AboutComponent],
+      
+    }).compileComponents();
+  })
+  
+  
+  beforeEach(()=>{
     fixture = TestBed.createComponent(AboutComponent);
-    component = fixture.componentInstance;
+    component =fixture.componentInstance;
     fixture.detectChanges();
-  });
-
-  it('should create', () => {
+  })
+  
+  it('should create', ()=>{
     expect(component).toBeTruthy();
   });
-});
+  });
+
